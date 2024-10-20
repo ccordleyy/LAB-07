8f5cd05961048c9ddeb7486c7408cccfe8bee179
Marvin Cordero Leyton <marvin_cordley@live.com>
2024-10-20 18:42:58 -0400

    Initial commit with React Vite setup

---
+++ react-example/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { List } from './components/List'
+import { WapperList } from './components/WrapperList'
+function App() {
+  
+  return (
+    <>
+    <WapperList>
+      <List/>
+    </WapperList>
+    </>
+  )
+}
+
+export default App
